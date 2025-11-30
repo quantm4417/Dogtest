d113fe29a9a642a70c47aaf0cf314ad9ed6a5316
quantm4417 <128858672+quantm4417@users.noreply.github.com>
2025-11-30 11:12:20 +0100

    request allow change

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # <--- CHANGE THIS
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
